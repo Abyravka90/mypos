e6ef1bc1f72554debf419659d1bc30d589e75493
Abyravka90 <asepcahya.nugraha75@gmail.com>
2024-05-27 21:33:13 +0700

    tutorial #5 session auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,4 +5,32 @@ class Auth extends CI_Controller {
 	{
 		$this->load->view('login');
 	}
+	public function process()
+	{
+		$post = $this->input->post(null, TRUE);
+		if(isset($_POST['login'])){
+			//call the model here
+			$this->load->model('User_m');
+			$query = $this->User_m->login($post);
+			if($query->num_rows() > 0){
+				//bedanya result dan rows kalau result hanya satu kalau result di extract semua
+				$row = $query->row();
+				$params =  array(
+					'userid' => $row->user_id,
+					'level' => $row->level
+				);
+				$this->session->set_userdata($params);
+				echo 
+				'<script>
+					alert("Selamat, login berhasil ");
+					window.location="'.site_url('dashboard').'";
+				</script>';
+			} else {
+				echo 
+				'<script>
+					alert("Login Gagal, Username / password salah ");
+					window.location="'.site_url('auth/login').'";
+				</script>';			}
+		}
+	}
 }
--- application/views/login.php
+++ application/views/login.php
@@ -24,13 +24,13 @@
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="" method="post">
+    <form action="<?= site_url('auth/process') ?>" method="post">
       <div class="form-group has-feedback">
         <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password" required>
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
@@ -41,7 +41,7 @@
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button name="login" type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
         </div>
         <!-- /.col -->
       </div>
